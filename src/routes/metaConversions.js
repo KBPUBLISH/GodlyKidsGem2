29a7ef52a4724855f7f3b6be1f9f9fdfe53ca35d
Midheal Bouchard <midhealbouchard@Midheals-Mac-mini.local>
2026-01-19 15:08:29 -0700

    Use App ID 713940524967938 for Meta tracking

--- src/routes/metaConversions.js
+++ src/routes/metaConversions.js
@@ -13,7 +13,7 @@ const crypto = require('crypto');
  * - META_ACCESS_TOKEN: Conversions API access token from Meta Events Manager
  */
 
-const PIXEL_ID = process.env.META_PIXEL_ID || '1372889104031125';
+const PIXEL_ID = process.env.META_PIXEL_ID || '713940524967938';
 const ACCESS_TOKEN = process.env.META_ACCESS_TOKEN;
 const API_VERSION = 'v18.0';
 const META_API_URL = `https://graph.facebook.com/${API_VERSION}/${PIXEL_ID}/events`;
